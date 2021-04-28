1bebfb02f740c52a0fcd34400eabe843203dec4a
tl-wajid-malik <61701789+tl-wajid-malik@users.noreply.github.com>
2021-04-28 10:18:17 +0100

    TypeScript and JavaScript examples (#4)
    * Add TypeScript example
    
    * Add javascript example
    
    * Use same packages
    
    * Add README
    
    * Change package
    
    * Set port variable

---
+++ v1/typescript/index.ts
@@ -0,0 +1,88 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import base64Url from 'base64url';
+import jwkToPem from 'jwk-to-pem';
+import { Algorithm, verify } from 'jws';
+import fetch from 'node-fetch';
+
+const port = 8080;
+const app = new Koa();
+const router = new Router();
+
+const expectedJwksUri: string = 'https://pay-api.truelayer.com/.well-known/jwks.json';
+
+const splitSignature = (signature: string): string[] => signature.split('.');
+
+const getJwsHeaders = (signature: string): JwsHeaders => {
+  const parts = splitSignature(signature);
+  return JSON.parse(base64Url.decode(parts[0]));
+}
+
+const verifyDetachedSignatureAsync = async (signature: string, payload: any): Promise<boolean> => {
+  const jwsHeaders = getJwsHeaders(signature);
+
+  const serializedPayload = JSON.stringify(payload);
+  const payloadB64 = base64Url.encode(serializedPayload);
+
+  const signatureParts = splitSignature(signature);
+  const fullSignature = `${signatureParts[0]}.${payloadB64}.${signatureParts[2]}`;
+
+  const jwksUri = jwsHeaders.jku;
+  if (jwksUri !== expectedJwksUri) {
+    return false;
+  }
+
+  const jwksResponse = await fetch(jwksUri);
+  const jwks: Jwks = await jwksResponse.json();
+
+  const key = jwks.keys.find(x => x.kid === jwsHeaders.kid);
+
+  const rsaPublicKey: string = jwkToPem(<jwkToPem.JWK>{
+    kty: 'RSA',
+    n: key.n,
+    e: key.e
+  });
+
+  return verify(fullSignature, <Algorithm>jwsHeaders.alg, rsaPublicKey);
+}
+
+router.post('events', '/events', async ctx => {
+  const signature = ctx.get('X-TL-Signature');
+  const payload = {
+    'Content-Type': 'application/json',
+    'X-TL-Webhook-Timestamp': ctx.get('X-TL-Webhook-Timestamp'),
+    'body': ctx.request.body
+  };
+  const verified = await verifyDetachedSignatureAsync(signature, payload);
+  ctx.status = verified ? 202 : 401;
+});
+
+
+app
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
+
+type Jwk = {
+  kty: string,
+  alg: string,
+  kid: string,
+  n: string,
+  e: string,
+  x5c: string[]
+}
+
+type Jwks = {
+  keys: Jwk[];
+}
+
+type JwsHeaders = {
+  alg: string,
+  kid: string,
+  jku: string,
+}
---
+++ v2/javascript/index.js
@@ -0,0 +1,65 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const base64Url = require('base64url');
+const jwkToPem = require('jwk-to-pem');
+const jws = require('jws');
+const fetch = require('node-fetch');
+
+const port = 8080;
+const app = new Koa();
+const router = new Router();
+
+const expectedJwksUri = 'https://pay-api.truelayer.com/.well-known/jwks.json';
+
+const splitSignature = (signature) => signature.split('.');
+
+const getJwsHeaders = (signature) => {
+  const parts = splitSignature(signature);
+  return JSON.parse(base64Url.decode(parts[0]));
+}
+
+const verifyDetachedSignatureAsync = async (signature, payload) => {
+  const jwsHeaders = getJwsHeaders(signature);
+
+  const serializedPayload = JSON.stringify(payload);
+  const payloadB64 = base64Url.encode(serializedPayload);
+
+  const signatureParts = splitSignature(signature);
+  const fullSignature = `${signatureParts[0]}.${payloadB64}.${signatureParts[2]}`;
+
+  const jwksUri = jwsHeaders.jku;
+  if (jwksUri !== expectedJwksUri) {
+    return false;
+  }
+
+  const jwksResponse = await fetch(jwksUri);
+  const jwks = await jwksResponse.json();
+
+  const key = jwks.keys.find(x => x.kid === jwsHeaders.kid);
+
+  const rsaPublicKey = jwkToPem({
+    kty: 'RSA',
+    n: key.n,
+    e: key.e
+  });
+
+  return jws.verify(fullSignature, jwsHeaders.alg, rsaPublicKey);
+}
+
+router.post('events', '/events', async ctx => {
+  const signature = ctx.get('X-TL-Signature');
+  const payload = ctx.request.body;
+  const verified = await verifyDetachedSignatureAsync(signature, payload);
+  ctx.status = verified ? 202 : 401;
+});
+
+
+app
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
